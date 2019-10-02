54902ac71277b781bba90e8e7a85c9fc0f89ac7c
opopop12 <55976798+opopop12@users.noreply.github.com>
2019-10-02 18:40:24 +0900

    Drills_04_완성
    2018180041 조해근
    animaition plus

---
+++ Drills/Drills_04/drill4.py
@@ -0,0 +1,37 @@
+from pico2d import *
+
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+x = 0
+frame = 0
+line = 1
+while True:
+    while x < 800:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, line * 100, 100, 100, x, 90)
+        update_canvas()
+        frame = (frame + 1) % 8
+
+        x += 5
+        delay(0.05)
+        get_events()
+
+    line = (line + 1) % 2
+
+    while x > 0:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, line * 100, 100, 100, x, 90)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x -= 5
+        delay(0.05)
+        get_events()
+
+    line = (line + 1) % 2
+
+close_canvas()
+
