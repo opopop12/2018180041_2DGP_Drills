b8ab9932ef2b1cb45c7341c13444a2561ec89ea5
opopop12 <55976798+opopop12@users.noreply.github.com>
2019-10-07 19:57:33 +0900

    애니메이션과 이동
    클릭시 애니메이션과 이동

--- Drills/Drill_05/Drill_05.py
+++ Drills/Drill_05/Drill_05.py
@@ -10,7 +10,6 @@ cursor = load_image('hand_arrow.png')
 
 move = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
-frame = 0
 hide_cursor()
 
 #def draw_line(p1: object, p2: object) -> object:
@@ -23,7 +22,36 @@ hide_cursor()
 #        update_canvas()
 #        frame = (frame + 1) % 8
 #    pass
+global x1,y1
+x1 = 100
+y1 = 100
+def move_to_cursor(p1: object, p2: object):
+    frame2 = 0
+    frame_line = 1
+    for i in range(0, 100+1, 2):
+        t = i/100
+        x1 = (1 - t) * p1[0] + t * p2[0]
+        y1 = (1 - t) * p1[1] + t * p2[1]
+        #character.clip_draw(frame * 100, 100 * 1, 100, 100, x1, y1)
+        clear_canvas()
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        cursor.draw(x, y)
+        if x-x1>0:
+            frame_line=1
+            character.clip_draw(frame2*100,frame_line*100,100,100,x1,y1)
+            update_canvas()
+            frame2 = (frame2 + 1) % 8
+        elif x-x1<0:
+            frame_line=0
+            character.clip_draw(frame2*100,frame_line*100,100,100,x1,y1)
+            update_canvas()
+            frame2 = (frame2 +1) %8
+        #character.clip_draw(0,0,100,100,x1,y1)
+        update_canvas()
+        delay(0.02)
+        handle_events()
 
+        #frame = (frame+1)%8
 
 def handle_events():
 
@@ -35,20 +63,21 @@ def handle_events():
             move = False
         elif event.type == SDL_MOUSEMOTION:
             x, y = event.x, KPU_HEIGHT - 1 - event.y
-        #elif event.type == SDL_MOUSEBUTTONDOWN:
-        #    draw_line(x,y)
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            move_to_cursor((x1,y1),(x,y))
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             move = False
 
     pass
 
+
 while move:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     cursor.draw(x,y)
+    #character.clip_draw(frame * 100, 0, 100, 100, x1, y1)
     update_canvas()
-    frame = (frame + 1) % 8
-
+    #frame = (frame + 1) % 8
     delay(0.02)
     handle_events()
 
